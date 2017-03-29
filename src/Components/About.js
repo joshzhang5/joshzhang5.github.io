17dfb4be4fd252aac7a03d5d1d148a790872e39d
Josh Zhang <joshzhang5@gmail.com>
2017-03-29 03:07:19 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,15 +15,13 @@ const style = {
   lineHeight: 1.75
 };
 
-const p1 = "Hey there! My name is Josh (Jiaxi) Zhang and I'm a second year studying Computer Science at the Georgia Institute of Technology." +
+const p1 = "Hey there! My name is Josh (Jiaxi) Zhang and I'm a student studying Computer Science at the Georgia Institute of Technology." +
 " I graduated in the top of my class from the largest high school in Ohio where I captained Mission Possible on the Science Olympiad" +
 " Team and ran Cross Country.";
 
 const p2 = "At Georgia Tech I conduct undergraduate research with Professor Ed Coyle, volunteer with various organizations," +
 " and am part of campus leadership initiatives" +
 " such as GT Leading Edge and AIESEC. As an active member of the College of Computing I serve as a department tutor and participate in serveral clubs such as the AI Club and GreyHat Hackers.";
-
-const p3 = "I plan to graduate with my Bachelors in 3 to 3.5 years and then pursue a Masters degree in Computer Science.";
 
 class About extends Component {
 
